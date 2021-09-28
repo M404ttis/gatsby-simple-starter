df6bce4e7ec21498031e32db01b38ad74d798696
Matthias LÃuecke <mattisluecke@gmail.com>
2021-09-28 10:11:04 +0200

    adds link tags to aboout.js and index.js for simple navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,9 @@ import React from "react"
 const About = () => (
   <div>
     <h1> This is the About page! 🌱</h1>
+    <div>
+      <a href="/">Home</a> <a href="/about"> About me</a>
+    </div>
     <p>
       Constantly trying to stay on track with the modern way of web Development
     </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,11 @@ import React from "react"
 const Home = () => (
   <div>
     <h1> This is the index page! ⚗️🧪</h1>
+
+    <div>
+      <a href="/">Home</a> <a href="/about"> About me</a>
+    </div>
+
     <p>
       Hi there, i am a junior software Developer, who fiddles around with 
       different projects using JavaScript, Html, Css and LIP.
